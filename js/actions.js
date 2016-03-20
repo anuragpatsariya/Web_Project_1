8efd03e42c1b4dd0b6239c4fb4641de570a8230e
Anurag Patsariya <anurag.patsariya@outlook.com>
2016-03-20 01:18:09 -0700

    Update actions.js

--- js/actions.js
+++ js/actions.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     //$('#myModal').modal();
     $('#myModal').on('shown.bs.modal', function() {
         $('#myInput').focus();
+        $("#submit")[0].disabled = false;
+        //$("#imgheading").val() = "";
+        //$("#imgdetails").val() = "";
     });
     $.ajax({
         url: "http://localhost:3000/imagedata",
@@ -24,40 +27,53 @@ var file_path_name;
 var imgheading;
 var imgdetails;
 function uploadimg() {
+    $("#submit")[0].disabled = true;
     var data = new FormData();
     //console.log(data.dataType);
     //console.log(data.type);
     $.each($('#imgfile')[0].files, function(i, file) {
-        
-        data.append("userphoto",file);
+
+        data.append("userphoto", file);
     });
     $.ajax({
-    url: 'api/photo',
-    data: data,
-    cache: false,
-    contentType: false,
-    processData: false,
-    type: 'POST',
-    success: function(data){
-    file_path_name = data;
-    console.log(file_path_name);
-    imgheading = $("#imgheading").val();
-    imgdetails = $("#imgdetails").val();
-    console.log(file_path_name,imgheading,imgdetails);
-    }
-});
+        url: 'api/photo',
+        data: data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        type: 'POST',
+        success: function(data) {
+            //alert("Photo uploaded successfully.");
+            $("#up").removeClass("sr-only");
+            file_path_name = data;
+            console.log(file_path_name);
+            imgheading = $("#imgheading").val();
+            imgdetails = $("#imgdetails").val();
+            console.log(file_path_name, imgheading, imgdetails);
+        },
+        error: function(xhr, status, error) {
+            alert("Photo upload failed.");
+            window.alert("Error: " + xhr.status + status + error);
+        }
+    });
 }
 
-function uploadRecord(){
-    console.log(file_path_name,imgheading,imgdetails);
+function uploadRecord() {
+    //$("#uploadrecord").disabled=true;
+    console.log(file_path_name, imgheading, imgdetails);
     $.ajax({
         url: "http://localhost:3000/imagedata",
         type: "POST",
-        data: {"img_path":file_path_name,"img_heading":imgheading,"img_details":imgdetails},
+        data: { "img_path": file_path_name, "img_heading": imgheading, "img_details": imgdetails },
         dataType: "JSON",
         success: function(data) {
-          var text =   "<li><figure> <img src=\"" + file_path_name + "\" alt=\"img0" + "" + "\" height=\"150\" width=\"50\"/><figcaption> <h3>" + imgheading + "</h3><p>" + imgdetails + "</p></figcaption> </figure></li>";
-          $("#grid")[0].innerHTML += text;
+            var text = "<li><figure> <img src=\"" + file_path_name + "\" alt=\"img0" + "" + "\" height=\"150\" width=\"50\"/><figcaption> <h3>" + imgheading + "</h3><p>" + imgdetails + "</p></figcaption> </figure></li>";
+            $("#grid")[0].innerHTML += text;
+            $("#imgfile")[0].value = null;
+            $("#imgheading")[0].value = "";
+            $("#imgdetails")[0].value = "";
+            new CBPGridGallery( document.getElementById( 'grid-gallery' ) );
+            
         },
         error: function(xhr, status, error) {
             window.alert("Error: " + xhr.status + status + error);
